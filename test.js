871fa94888a79a12fef4afbf76121d473b4a0b84
DevMonkeyster <33471573+DevMonkeyster@users.noreply.github.com>
2021-01-11 16:09:26 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
 
+
 const body = {
-  "nickname" : "蛋壳我是你父亲啊哈哈哈",
-  "timestamp" : "1610351312"
+    "nickname" : "蛋壳我是你父亲啊哈哈哈",
+    "timestamp" : "1610351312"
 }
+
+console.log(document.body);
+
+console.log(body);
