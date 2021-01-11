26fd8e62632e4a958a83bedc773773c6992b6cf0
DevMonkeyster <33471573+DevMonkeyster@users.noreply.github.com>
2021-01-11 16:29:50 +0800

    Update test.js
    body change

--- test.js
+++ test.js
@@ -1,15 +1,10 @@
 
-
-const body = {
-    "nickname" : "蛋壳我是你父亲啊哈哈哈",
-    "timestamp" : "1610351312"
-}
-console.log(body);
+console.log($request.body);
 
 const bodydic = $request.body;
 bodydic["nickname"] = "我是蛋壳的亲爹";
 
-
+$request.body = bodydic;
 
 console.log($request.body);
 
